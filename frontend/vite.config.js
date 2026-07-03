7a876025aea9da4654c0c40b72c9465e5b055efd
Arpitnew2322 <arpitmasharma@gmail.com>
2026-07-03 17:05:05 +0530

    Fix Vercel base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   plugins: [
     tailwindcss(),
-  
   ],
-  base:"First-E-Commerce",
-})  
+})
