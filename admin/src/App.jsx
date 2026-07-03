ae433eb4d185c9fd4c1115daa641a90dd6d6eb81
Arpitnew2322 <arpitmasharma@gmail.com>
2026-07-03 17:34:47 +0530

    Fix Orders import

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,7 +4,7 @@ import Sidebar from './components/Sidebar'
 import { Routes, Route } from 'react-router-dom'
 import Add from './pages/Add'
 import List from './pages/List'
-import Orders from './pages/orders'
+import Orders from './pages/Orders'
 import { useState ,useEffect } from 'react'
 import Login from './components/Login'
 import { ToastContainer } from 'react-toastify';
